6f4b8fd796200e5bbe420c7b40e95c921873f146
ErickD99 <80652545+ErickD99@users.noreply.github.com>
2021-03-18 20:40:49 -0300

    Create prog03.py

---
+++ prog03.py
@@ -0,0 +1,34 @@
+num_1 = -3
+num_2 = 3.14
+
+#Operações
+print(type(num_1), type(num_2))
+print(num_1 + num_2)
+print(num_1 - num_2)
+print(num_1 * num_2)
+print(num_1 / num_2)
+print(num_1 // num_2)
+print(num_1 ** num_2)
+print(num_1 % num_2)
+
+num_1 = num_1 + 1
+num_1 += 1 #Funciona para qualquer operação
+
+print(abs(num_1))
+print(round(num_2, 1))
+
+#Comparadores
+print(num_1 == num_2)
+print(num_1 != num_2)
+print(num_1 > num_2)
+print(num_1 < num_2)
+print(num_1 >= num_2)
+print(num_1 <= num_2)
+
+num_1 = '-3'
+num_2 = '3.14'
+
+num_1 = int(num_1)
+num_2 = float(num_2)
+
+print(num_1 + num_2)
